923b7babd20a509c27df883848f036e5f07015de
petar_veljovic <petarveljovic1234@gmail.com>
2022-10-17 22:26:32 -0300

    Add files via upload
    add headers

---
+++ func.h
@@ -0,0 +1,10 @@
+#ifndef __func_h_			/* Pergunta se existe essa biblioteca */
+#define __func_h_			/* Faz existir */
+
+#include "main.h"
+#include "func.h"
+
+int		ehPrimo(int numero);
+void	gerarMatriz();
+
+#endif  
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef __main_h_                       /* Pergunta se existe existe essa biblioteca */
+#define __main_h_                       /* Faz existir */
+
+int main (int argc, char **argv);       /* Insere-se as funcoes do codigo na biblioteca */
+
+#endif                                  /* Termina */
