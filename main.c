8b571c6d1011270e5e47c203a49fed9087c1230d
petar_veljovic <petarveljovic1234@gmail.com>
2022-10-17 22:22:27 -0300

    Add files via upload
    added headers

---
+++ main.c
@@ -0,0 +1,34 @@
+/*
+  %%%  Instituicao:     IFES
+  %%%  Curso:           BSI
+  %%%  Disciplina:      SO
+  %%%  Tipo:            Trabalho 1
+  %%%  Ano:             2022
+  %%%  versao           001.002
+*/
+
+/* Declaração das bibliotecas internas*/
+#pragma comment(lib,"pthreadVC2.lib")
+#define HAVE_STRUCT_TIMESPEC
+
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS 1
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1
+#pragma comment(lib,"pthreadVC2.lib")
+#define HAVE_STRUCT_TIMESPEC
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+/* Declaração das bibliotecas externas*/
+#include "main.h"
+#include "func.h"
+
+int main(int argc, char** argv)
+{
+    gerarMatriz();
+    return 0;
+}
